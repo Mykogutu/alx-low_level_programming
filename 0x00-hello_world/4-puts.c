0e14030c9f7da02ac16bc5734923cf9d13d3bb26
Mykogutu <ogutu47@gmail.com>
2022-10-14 17:07:51 -0700

    using puts to output a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * main - This is the main function
+ * Description - The function uses put
+ * to output a sentence
+ */
+
+int main(void)
+{
+	char strng[51];
+
+	strcpy(strng, "Programming is like building a multilingual puzzle");
+	puts(strng);
+	return (0);
+}
