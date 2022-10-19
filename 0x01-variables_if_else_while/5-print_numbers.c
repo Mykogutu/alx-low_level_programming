3f0581ae2a6ec7a7ca196a6571222be22b9cb96a
Mykogutu <ogutu47@gmail.com>
2022-10-18 20:36:35 -0700

    printing 1-10 using for loops

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints single gitits numbers
+ * Return: return 0 if successful.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+	return (0);
+}
