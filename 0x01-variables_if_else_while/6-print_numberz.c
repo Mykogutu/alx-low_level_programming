e380b55e24f31fb0feb035d9feeed7a18adcf4b8
Mykogutu <ogutu47@gmail.com>
2022-10-19 16:22:25 -0700

    printing all single digits starting from 0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -prints all single digit numbers
+ * of base 10 starting from 0
+ * Return: returns 0 is success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num < '9'; num++)
+	{
+		putchar(num)
+	}
+	putchar('\n');
+	return (0);
+}
