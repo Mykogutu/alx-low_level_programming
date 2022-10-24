d721fa03bbadd7a85edcccba6ed77089911ce0ee
Mykogutu <ogutu47@gmail.com>
2022-10-24 04:55:19 -0700

    Printing squares using for loop

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - print squares
+ * @size: the number of squares
+ *
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
