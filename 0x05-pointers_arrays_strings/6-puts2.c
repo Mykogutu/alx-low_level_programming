cd86e696eed7bc119aff6e59586591365244ef72
Mykogutu <ogutu47@gmail.com>
2022-10-25 12:19:38 -0700

    printing every character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 - prints every other character of a string
+ * with the first character followed by a new line
+ * @str: string to print
+ *
+ * Return: Always 0
+ */
+void puts2(char *str)
+{
+	int i;
+	int len = strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		printf("%c\n", str[i]);
+	}
+}
