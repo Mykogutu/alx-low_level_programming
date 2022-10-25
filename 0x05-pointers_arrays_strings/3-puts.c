6711282f8339724ae572b44942cdfbbb26091a16
Mykogutu <ogutu47@gmail.com>
2022-10-25 06:56:53 -0700

    a function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - prints strings
+ * @str: string input
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
