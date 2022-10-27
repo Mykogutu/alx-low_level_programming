d9dfa2c63c1db9c9a1b390c0479d83f4ad7b23fb
Mykogutu <ogutu47@gmail.com>
2022-10-27 05:07:02 -0700

    function that concatenates two strings and uses n bytes from src

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: Destination string
+ * @src: Source string
+ * @n: number of characters
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int size1 = strlen(dest);
+
+	for (i = 0; i < n; i++)
+	{
+		dest[size1 + i] = src[i];
+	}
+	return (dest);
+}
