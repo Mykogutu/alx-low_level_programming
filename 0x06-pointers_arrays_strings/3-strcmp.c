d0d9766cf943f4b878807e0f1637d3946d46ffc6
Mykogutu <ogutu47@gmail.com>
2022-10-28 01:06:23 -0700

    string compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: First string
+ * @s2: Second string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] == s2[i]; i++)
+		if (s2[i] == '\0')
+		{
+			return (0);
+		}
+	return (s1[i] - s2[i]);
+}
+
+
