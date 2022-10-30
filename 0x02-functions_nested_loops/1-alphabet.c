75fc52e2ec1505fcd995f867c9a6b6a36c12a116
Mykogutu <ogutu47@gmail.com>
2022-10-30 00:46:08 -0700

    printing lowercase alphabetswq

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - prints lowercase alphabets
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = '0'; i <= '122'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
