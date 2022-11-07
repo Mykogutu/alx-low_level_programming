fde1a37ee889b2068841d64b90c15d2838c26fb9
Mykogutu <ogutu47@gmail.com>
2022-11-07 02:17:45 -0800

    correction to betty style guide

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,12 @@
 
 /**
  * main - prints the nam of the program
- *
+ * @argc: number of arguments
+ * @argv: array of CLI arguments passed
  * Return: Always 0
  */
 int main(int argc, char *argv[])
 {
-	
 	(void) argc;
 	printf("%s\n", argv[0]);
 	return (0);
