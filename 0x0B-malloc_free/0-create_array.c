414bd8da55c79ae8c9b28539eb439ab549102947
Mykogutu <ogutu47@gmail.com>
2022-11-08 23:54:27 -0800

    betty style correction

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 	}
 	else
 	{
-		arr = (char *)malloc(sizeof(char)*size);
+		arr = (char *)malloc(sizeof(char) * size);
 		if (arr != NULL)
 		{
 			for (i = 0; i < size; i++)
