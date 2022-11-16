a307647c94f42d979563c6910c61707c0326fd9b
Mykogutu <ogutu47@gmail.com>
2022-11-16 11:04:23 -0800

    a new type dog_t as a new name for the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,9 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/**
+ * dog - struct with typedef
+ */
+typedef struct dog dog_t;
 #endif /* DOG_H */
