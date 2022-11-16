42a6f7bdcb7169ac4f94926691508ead8caf00f0
Mykogutu <ogutu47@gmail.com>
2022-11-16 11:59:03 -0800

    a function that frees memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_dog - frees memory
+ * @d: pointer to struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
