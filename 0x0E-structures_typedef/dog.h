aada2246949061fe176f72ccbf7cb6f87005e8e0
Mykogutu <ogutu47@gmail.com>
2022-11-16 08:58:54 -0800

    defining a struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * struct dog - self declared data type
+ * @name: First member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: a struct data type with three variables
+ */
+struct dog
+{
+	char *name;
+	double age;
+	char *owner;
+};
